c8819a36b2a004cfb60b801f1fe6a60f4713d49f
Richard Albert Leddy <richardaleddy@gmail.com>
2021-09-05 16:44:33 -0700

    Move files to Registry. Descend from registry

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const DirWatcherHandler = require('./lib/directory_watch_handler.js')
-const ObjFileDirLoader = require('./lib/object_file_loader.js');
+const {DirWatcherHandler,ObjFileDirLoader} = require('copious-registry')
+//
 const SearchesByUser = require('./lib/single_owner_searches.js');
 const {Searching} = require('./lib/searching.js')
-
 //
 module.exports.DirWatcherHandler = DirWatcherHandler
 module.exports.ObjFileDirLoader = ObjFileDirLoader
